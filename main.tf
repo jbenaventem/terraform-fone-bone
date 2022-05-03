610c29c02cce9439c9649b61902bcffbfd0e3395
jbenaventem <jesus.benavente@gruposantander.com>
2022-05-03 11:48:00 +0200

    Include List Commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  cloud {
+    organization = "test-cto"
+
+    workspaces {
+      name = "terraform-fone-bone.dev"
+    }
+  }
+}
+resource "random_pet" "generate_id" {
+  length    = 1
+  prefix    = "Fone Bone"
+  separator = "."
+}
+
+resource "local_file" "comic" {
+  filename = "./mis-comic.txt"
+  content  = "01 - Lejos de Boneville \n02 - La gran carrera de Vacas \n"
+}
