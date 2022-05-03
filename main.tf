8acab8d6d041e488071803f43bfed4e6651d2544
Jesús Benavente Martínez <jesus.benavente@gruposantander.com>
2022-05-03 16:00:17 +0200

    PR Improve ci-cd (#17)
    * Test PR main cloud provider
    
    * Tes01
    
    * Tes01 (#11)
    
    * 03-Los ojos de la tormenta
    
    * Feature/test (#12)
    
    * Tes01
    
    * 03-Los ojos de la tormenta
    
    * El Matadragones
    
    * Rock Jaw, Señor de la frontera oriental
    
    * Include ci-on-feature-branch
    
    * Include terraform remote backend
    
    * Include terraform remote backend
    
    * Include terraform remote backend
    
    * Include terraform remote backend
    
    * Include terraform remote backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  cloud {
+    organization = "test-cto"
+
+    workspaces {
+      name = "terraform-fone-bone-vcs"
+    }
+  }
+}
 resource "random_pet" "generate_id" {
   length    = 1
   prefix    = "Fone Bone"
