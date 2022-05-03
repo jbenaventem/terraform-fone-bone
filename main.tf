f62eb5ca109a5c6777f107b9b512eb693aebef44
jbenaventem <jesus.benavente@gruposantander.com>
2022-05-03 13:28:20 +0200

    El Matadragones

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ resource "random_pet" "generate_id" {
 
 resource "local_file" "comic" {
   filename = "./mis-comic.txt"
-  content  = "01 - Lejos de Boneville \n02 - La gran carrera de Vacas \n03 - Los ojos de la tormenta"
+  content  = "01 - Lejos de Boneville \n02 - La gran carrera de Vacas \n03 - Los ojos de la tormenta \n04 -  El Matadragones"
 }
