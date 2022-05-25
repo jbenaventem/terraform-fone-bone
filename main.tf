7d9b8abe7b03fe72027d100d9d00707d2e88a0e0
Jesús Benavente Martínez <jesus.benavente@gruposantander.com>
2022-05-25 18:47:02 +0200

    feat:secret-test (#20)

--- main.tf
+++ main.tf
@@ -15,5 +15,6 @@ resource "random_pet" "generate_id" {
 
 resource "local_file" "comic" {
   filename = "./mis-comic.txt"
-  content = "${var.content}  --  ${var.password}" 
+  //content = "${var.content}  --  ${var.password}" 
+  content = "Test"
 }
