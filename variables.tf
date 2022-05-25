38fde8c0cc7887497e5933cb1a68de8bd0dc56c6
Jesús Benavente Martínez <jesus.benavente@gruposantander.com>
2022-05-25 19:48:11 +0200

    Change env vars (#25)

--- variables.tf
+++ variables.tf
@@ -1,10 +1,8 @@
 variable "content" {
   description = "Content DB2"
   type        = string
-  default     = "Content"
 }
 variable "password" {
   description = "The password for anything"
   type        = string
-  default     = "12345678"
 }
