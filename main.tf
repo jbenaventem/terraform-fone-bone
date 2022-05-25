eb05b6af735c7d4a13da49899137b5ce0c1fc40d
jbenaventem <jesus.benavente@gruposantander.com>
2022-05-25 20:01:46 +0200

    Include cloud

--- main.tf
+++ main.tf
@@ -1,12 +1,22 @@
-#terraform {
-#  cloud {
-#    organization = "test-cto"
+terraform {
+  cloud {
+    organization = "test-cto"
+    workspaces {
+      name = "terraform-fone-bone-dev"
+    }
+  }
+}
 
-#    workspaces {
-#      name = "terraform-fone-bone-dev"
-#    }
-#  }
-#}
+variable "content" {
+  type        = string
+  default     = "Contenido de mi fichero"
+  sensitive   = true
+}
+variable "password" {
+  type        = string
+  default     = "EstoEsUnaPassword"
+  sensitive   = true
+}
 
 resource "random_pet" "generate_id" {
   length    = 1
--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "content" {
-  description = "Content DB2"
-  type        = string
-}
-variable "password" {
-  description = "The password for anything"
-  type        = string
-}
